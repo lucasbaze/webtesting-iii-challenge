5b5374be0c854bb57a892e2fc7f90807e2bd7746
Lucas Bazemore <lucasabazemore@gmail.com>
2019-09-16 16:58:08 -0500

    no idea what I'm doing. Very frustrated. Did some test... will do more... I guess

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library';
+import renderer from 'react-test-renderer';
+import { mount, shallow } from 'enzyme';
+
+import Controls from './Controls';
+
+test('provide buttons to toggle the closed and locked states.', () => {
+    expect(shallow(<Controls />)).toMatchSnapshot();
+});
